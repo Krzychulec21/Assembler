dc8c5b74b0b9e0e868a23d8aded5ad2983619208
Krzychulec21 <krzychu123g@gmail.com>
2023-03-31 20:06:38 +0200

    ex_2 done

---
+++ lab1/size2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("size.c\n\n");
+    
+    printf("size of(unsigned char) = %d\n", sizeof(unsigned char));
+    printf("size of(unsigned short) = %d\n", sizeof(unsigned short));
+    printf("size of(unsigned int) = %d\n", sizeof(unsigned int));
+    printf("size of(unsigned long) = %d\n", sizeof(unsigned long));
+    printf("size of(unsigned long int) = %d\n", sizeof(unsigned long int));
+    printf("size of(unsigned long long) = %d\n", sizeof(unsigned long long));
+    
+    return 0;
+
+}
